8beeac769b1c1dd46fe672a41b51fad30aef7be3
Masterov_IN <masterov_in@mail.ru>
2022-09-06 18:57:15 +0300

    Task66 done

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -5,5 +5,32 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
+void ElemSumm1(int x, int y, int sum)
+{    
+    sum+=y;
+    if(y<x)
+    {
+    Console.Write($"Сумма элементов = {sum}");
+    return;
+    }
+    ElemSumm1(x,y-1,sum);
+}
 
+void ElemSumm2(int x, int y, int sum)
+{    
+    sum+=x;
+    if(x<y)
+    {
+    Console.Write($"Сумма элементов = {sum}");
+    return;
+    }
+    ElemSumm2(x-1,y,sum);
+}
+
+if (m<n) ElemSumm1(m,n,0);
+else ElemSumm2(m,n,0);
