192b57a0c3a094965d0ccc03b92c994e939a4b12
Masterov_IN <masterov_in@mail.ru>
2022-09-06 21:29:37 +0300

    Task68 done

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -4,6 +4,18 @@
 // Даны два неотрицательных числа m и n.
 // m = 3 n = 2 -> A(m,n) = 29
 
+int m = 3;
+int n = 3;
+
+int Ackerman(int x, int y)
+{
+  if (x == 0) return y + 1;
+  else if (y == 0) return Ackerman(x - 1, 1);
+  else return Ackerman(x - 1, Ackerman(x, y - 1));
+}
+
+int result = Ackerman(m,n);
+Console.Write($"A({m},{n}) = {result}");
 
 
 
